c8ba659941a272df4f26ee1837fa32e4d6009716
Daniel Deutsch <deudan1010@gmail.com>
2016-12-07 14:59:22 +0100

    set up basic server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');//HTTP request logger
+var bodyParser = require('body-parser');//For parsing POST request data
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
