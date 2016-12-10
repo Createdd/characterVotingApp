286506f0644845cf2e43c6a6756482b8649ffd55
Daniel Deutsch <deudan1010@gmail.com>
2016-12-10 10:46:01 +0100

    update app.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Footer from './Footer.jsx';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render(){
     return(
+      <div>
+        <Navbar history={this.props.history} />
+        {this.props.children}
+        <Footer />
+      </div>
       <div>
         {this.props.children}
         <Footer />
